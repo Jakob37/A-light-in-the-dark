6fe8f697e1e96a4ae266f3bd303a4e7140ff2ee7
Jakob Willforss <jakob.willforss@hotmail.com>
2022-12-17 20:22:00 +0100

    Basic torch for player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,37 +5,40 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    // Transform transform;
+    private Torch torch;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // GameObject player = GameObject.Find("Player");
-        // GameObject go = this.gameObject;
-        // Access player's transform
-        // this.transform = go.transform;
+        this.torch = this.GetComponent<Torch>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Move on arrow key press
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.torch.resetLight();
+        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             this.transform.position = new Vector2(
                 this.transform.position.x + 0.1f, this.transform.position.y
             );
-        } else if (Input.GetKey(KeyCode.LeftArrow))
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             this.transform.position = new Vector2(
                 this.transform.position.x - 0.1f, this.transform.position.y
             );
-        } else if (Input.GetKey(KeyCode.UpArrow))
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
         {
             this.transform.position = new Vector2(
                 this.transform.position.x, this.transform.position.y + 0.1f
             );
-        } else if (Input.GetKey(KeyCode.DownArrow))
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             this.transform.position = new Vector2(
                 this.transform.position.x, this.transform.position.y - 0.1f
---
+++ Assets/Torch.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Torch : MonoBehaviour
+{
+
+    public float fullDuration = 5;
+    public float fullLightRadius = 6;
+    private int currLightRadius = 0;
+    private float currDuration = 5;
+
+    private Light2D light2D;
+
+    public void resetLight()
+    {
+        this.currDuration = this.fullDuration;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.light2D = this.GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currDuration > 0)
+        {
+            currDuration -= Time.deltaTime;
+        }
+
+        if (currDuration > 0)
+        {
+            this.light2D.intensity = 1;
+        }
+        else
+        {
+            this.light2D.intensity = 0;
+        }
+
+        this.light2D.pointLightOuterRadius = this.fullLightRadius * (currDuration / fullDuration);
+    }
+}
